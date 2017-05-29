a440a58b0ee53380311e04e22b1d5cf1011434f7
eliassilfer <eliassilfer@gmail.com>
2017-05-29 16:49:38 -0300

    tela cadastro

--- src/main/java/com/catalogo/controller/ItemController.java
+++ src/main/java/com/catalogo/controller/ItemController.java
@@ -1,7 +1,10 @@
 package com.catalogo.controller;
 
 import com.catalogo.domain.Item;
+import com.catalogo.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,8 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ItemController {
 
+    @Autowired
+    private ItemRepository itemRepository;
+
     @RequestMapping("/index")
-    public String index(@ModelAttribute Item item) {
+    public String index(Model model) {
+        model.addAttribute("item", new Item());
+        return "index";
+    }
+
+    @RequestMapping("/cadastrar")
+    public String cadastrar(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        for (Item i : itemRepository.findAll()) {
+            System.out.println(i.toString());
+        }
         return "index";
     }
 }
